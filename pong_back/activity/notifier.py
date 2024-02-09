137dd748b7bfd5b0c1d69f4dccac1bd26e4c4041
ethaaalpha <revenus-nager-0s@icloud.com>
2024-02-09 15:41:41 +0100

    websocket using sync is ok

--- pong_back/activity/notifier.py
+++ pong_back/activity/notifier.py
@@ -8,21 +8,9 @@ class ActivityNotifier():
 	"""
 	Use to notify client using WebSocket
 	-----
-	sendPrivateMessage(args: dict) ['from', 'to', 'content'] \n
 	sendFriendRequest(sender: str, target: str):
 	"""
-
-	@staticmethod
-	def sendPrivateMessage(args: dict):
-		required = ['from', 'to', 'content']
-
-		# security checks of values inside of Websocket send
-		if any(need not in args.keys() for need in required):
-			return
-		if any(key not in required for key in args.keys()):
-			return
-		ActivityNotifier._notifyBuilder(args['from'], args['to'], args, 'chat')
-
+	
 	@staticmethod
 	def sendFriendRequest(senderName: str, targetName: str):
 		content = {
@@ -39,19 +27,19 @@ class ActivityNotifier():
 		target = Profile.getUserFromUsername(target)
 
 		# Prevent from inexisting user
-		if not sender or not target:
+		if not sender or not target or not content:
 			return
 		
 		# Blocklist of user
 		if target.profile.is_block(sender) or sender.profile.is_block(target):
 			return
-		
 		ActivityNotifier._notify(getChannelName(target), content, event)
 
 	@staticmethod
 	def _notify(channel: str, content: str, event: str, type='send.message'):
 		"""
 		Do not use without _notifyBuilder
+		Only from a SYNC context
 		"""
 		async_to_sync(channel_layer.group_send)(channel, {
 			"type" : type,
