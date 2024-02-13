703957c39a6d689950167edd42e178f2a73ed1a9
ethaaalpha <revenus-nager-0s@icloud.com>
2024-02-13 15:02:08 +0100

    before merge

--- pong_back/home/views.py
+++ pong_back/home/views.py
@@ -3,9 +3,7 @@ from django.http import HttpResponse, HttpRequest
 
 # Create your views here.
 def index(request: HttpRequest):
-	if (request.user.is_authenticated):
-		return(render(request, "index.html"))
-	return (render(request, 'unlogged.html'))
+	return(render(request, "index.html"))
 
 def game(request: HttpRequest):
 	return (render(request, 'game.html'))
