59d6fe4595dfddcc294d3d311804964b3dc1d839
ethaaalpha <revenus-nager-0s@icloud.com>
2024-02-21 13:20:38 +0100

    working on matchamaking and grouppps

--- pong_back/coordination/matchmaking.py
+++ pong_back/coordination/matchmaking.py
@@ -10,7 +10,8 @@ class Matchmaking:
 
 		if user not in Matchmaking._stack:
 			Matchmaking._stack.append(user)
-
+		else:
+			return ("Already inside the queue !")
 		if len(Matchmaking._stack) >= 2: # Run when match 2 players
 			playerA: User = Matchmaking._stack[-1]
 			playerB: User = Matchmaking._stack[-2]
@@ -23,8 +24,11 @@ class Matchmaking:
 			if (room.addPlayer(playerB)) == 0:
 				room.sendMessageNext(playerA, playerB)
 				room.sendMessageNext(playerB, playerA)
-
+		return ("Successfully added to the matchmaking queue !")
+	
 	@staticmethod
 	def removePlayerToQueue(user: User):
 		if user in Matchmaking._stack:
 			Matchmaking._stack.remove(user)
+			return ("Successfully added to the matchmaking queue !")
+		return ("User weren't in matchmaking !")
