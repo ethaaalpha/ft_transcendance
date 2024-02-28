747e8d1624326e1c7df5d039edf128f66451d6d9
ethaaalpha <revenus-nager-0s@icloud.com>
2024-02-28 16:41:00 +0100

    update js

--- pong_front/websocket.js
+++ pong_front/websocket.js
@@ -3,12 +3,12 @@ class activityWebsocket {
 	connect() {
 		this.socket = new WebSocket('wss://' + window.location.host + '/api/activity/');
 		
-		this.socket.onopen = function(e) {
+		this.socket.onopen = (e) => {
 			console.log("Le websocket 'activity' est bien connecté !")
 		}
 		
 		// Juste pour debug
-		this.socket.onmessage = function(e) {
+		this.socket.onmessage = (e) => {
 			const data = JSON.parse(e.data);
 			const stringifiedData = JSON.stringify(data)
         	document.querySelector('#activity-log').value += stringifiedData +'\n';
@@ -16,22 +16,21 @@ class activityWebsocket {
 	}
 
 	registerEvents() {
-		this.socket.onclose = function(e) {
+		this.socket.onclose = (e) => {
 			console.error('Chat socket closed unexpectedly ! Retrying to connect !');
-			setTimeout(function() {
-				connect();
+			setTimeout(() => {
+				this.connect();
 			}, 1000);
 		};
 		this.catchEventHTML();
 	}
 
 	catchEventHTML() {
-		document.querySelector('#chat-message-submit').onclick = function(e) {
+		document.querySelector('#chat-message-submit').onclick = (e) => {
 			const messageInputDom = document.querySelector('#chat-message-input');
 			const targetInputDom = document.querySelector('#chat-message-target');
-			data = {'from': username, 'to': targetInputDom.value, 'content': messageInputDom.value}
-			console.log(`J'envoie ça ${JSON.stringify(data)}`)
-			chatSocket.send(JSON.stringify({
+			const data = {'from': username, 'to': targetInputDom.value, 'content': messageInputDom.value}
+			this.socket.send(JSON.stringify({
 				'event': 'chat',
 				'data': data,
 			}));
@@ -45,11 +44,11 @@ class coordinationWebsocket {
 	connect() {
 		this.socket = new WebSocket('wss://' + window.location.host + '/api/coordination/');
 
-		this.socket.onopen = function(e) {
+		this.socket.onopen = (e) => {
 			console.log("Le websocket 'coordination' est bien connecté !")
 		}
 
-		this.socket.onmessage = function(e) {
+		this.socket.onmessage = (e) => {
 			const data = JSON.parse(e.data);
 			const stringifiedData = JSON.stringify(data)
 			document.querySelector('#activity-log').value += stringifiedData +'\n';
@@ -57,29 +56,29 @@ class coordinationWebsocket {
 	}
 
 	registerEvents() {
-		this.socket.onclose = function(e) {
+		this.socket.onclose = (e) => {
 			console.error('Chat socket closed unexpectedly ! Retrying to connect !');
-			setTimeout(function() {
-				connect();
+			setTimeout(() => {
+				this.connect();
 			}, 1000);
 		};
 		this.catchEventHTML();
 	}
 
 	catchEventHTML() {
-		document.querySelector("#button-match").onclick = function(e) {
-			coordinationSocket.send(JSON.stringify({'event' : 'matchmaking', 'data' : {'action': 'join'}}))
+		document.querySelector("#button-match").onclick = (e) => {
+			this.socket.send(JSON.stringify({'event' : 'matchmaking', 'data' : {'action': 'join'}}))
 		}
-		document.querySelector("#button-join").onclick = function(e) {
+		document.querySelector("#button-join").onclick = (e) => {
 			domInput = document.querySelector("#room-id-input");
 			value = domInput.value;
-			coordinationSocket.send(JSON.stringify({'event' : 'tournament', data: {'room-id': value, 'action': 'join'}}));
+			this.socket.send(JSON.stringify({'event' : 'tournament','data': {'room-id': value, 'action': 'join'}}));
 			domInput.value = "";
 		}
-		document.querySelector("#button-create").onclick = function(e) {
+		document.querySelector("#button-create").onclick = (e) => {
 			domInput = document.querySelector("#room-id-input");
 			value = domInput.value;
-			coordinationSocket.send(JSON.stringify({'event' : 'create', data: {'mode': value}}));
+			this.socket.send(JSON.stringify({'event' : 'create', 'data': {'mode': value}}));
 			domInput.value = "";
 		}
 	}
