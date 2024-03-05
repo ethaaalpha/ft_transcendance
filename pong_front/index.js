0b93d0485c51801a469016eee2b231b527bc5419
ethaaalpha <revenus-nager-0s@icloud.com>
2024-03-05 16:24:25 +0100

    spa test update + add in-game chat

--- pong_front/index.js
+++ pong_front/index.js
@@ -2,7 +2,6 @@
 function handleFormSubmit(event) {
 	event.preventDefault();
 
-
 	const form = event.currentTarget;
 	// Create a FormData object for the form
 	const formData = new FormData(form);
@@ -15,6 +14,25 @@ function handleFormSubmit(event) {
 	fetchData(apiUrl, method, formData)
 		.then(data => {
 			console.log('Data:', data);
+		},
+		error => console.error('Error:', error))
+}
+
+function handleReloader(event) {
+	event.preventDefault();
+
+	const form = event.currentTarget;
+	// Create a FormData object for the form
+	const formData = new FormData(form);
+
+	// Now you can proceed with form submission using fetchData or other logic
+	console.log(`ici ${form.getAttribute('action')}`)
+	const apiUrl = form.getAttribute('action');  // Use the form's action attribute as the API endpoint
+	const method = form.method.toUpperCase();
+
+	fetchData(apiUrl, method, formData)
+		.then(data => {
+			pageElement.reload();
 		},
 		error => console.error('Error:', error))
 }
@@ -49,8 +67,7 @@ function loadLogged() {
 		(async () => {
 			await fetchData('/api/auth/logout', 'POST')
 				.then(data => {
-					changeClass('logged', 'none');
-					changeClass('unlogged', 'block');
+					pageElement.reload();
 				});
 		})();
 	};;
@@ -85,24 +102,40 @@ async function isLogged() {
 	return response.status === 200;
 }
 
-async function main() {
-	changeClass('logged', 'none');
-	changeClass('unlogged', 'none');
-
-	isLogged = await isLogged();
-	if (isLogged) {
-		console.log("utilisateur connecté")
-		loadLogged();
-	}
-	else {
-		console.log("utilisateur déconnecté")
-		loadUnlogged();
+class Page{
+	constructor() {
+		this.isLogged = false;
+		changeClass('logged', 'none');
+		changeClass('unlogged', 'none');
 	}
 
-	const ajaxForms = document.querySelectorAll('.ajax-form');
-    ajaxForms.forEach(form => {
-        form.addEventListener('submit', handleFormSubmit);
-    });
+	load() {
+		const ajaxForms = document.querySelectorAll('.ajax-form');
+		ajaxForms.forEach(form => {
+			form.addEventListener('submit', handleFormSubmit);
+		});
+
+		const reloaderForms = document.querySelectorAll('.reloader');
+		reloaderForms.forEach(form => {
+			form.addEventListener('submit', handleReloader);
+		});
+		this.reload();
+	} 
+
+	async reload() {
+		changeClass('logged', 'none');
+		changeClass('unlogged', 'none');
+		this.isLogged = await isLogged();
+		if (this.isLogged) {
+			console.log("utilisateur connecté")
+			loadLogged();
+		}
+		else {
+			console.log("utilisateur déconnecté")
+			loadUnlogged();
+		}
+	}
 }
 
-main();
+pageElement = new Page();
+pageElement.load();
