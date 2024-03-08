6a4293c7d868999c2f1b68c93cfead63685c1894
ethaaalpha <revenus-nager-0s@icloud.com>
2024-03-08 13:43:19 +0100

    threejs auto download local fix

---
+++ docker/nginx/runner.sh
@@ -0,0 +1,16 @@
+THREEJS_ARCHIVE="r162.tar.gz"
+THREEJS_URL="https://github.com/mrdoob/three.js/archive/refs/tags/r162.tar.gz"
+THREEJS_DIR="three"
+
+if [ ! -d "/usr/share/libs/$THREEJS_DIR" ]; then
+    echo "Downloading threejs"
+    wget "$THREEJS_URL" -O "$THREEJS_ARCHIVE"
+    tar -xvzf "$THREEJS_ARCHIVE"
+    mv "three.js-r162/" "$THREEJS_DIR"
+    mv "$THREEJS_DIR" /usr/share/libs/
+else
+    echo "Threejs archive already exists. Skipping download."
+fi
+
+echo "Running nginx"
+nginx -g "daemon off;"
