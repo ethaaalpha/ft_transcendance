a04dcf8611142553bb37e29ed84e21756edb7d6f
Mehdi Desmartin <mdesmartin@mehdis-air.home>
2024-03-13 14:53:50 +0100

    test

--- front_mehdi/assets/js/script.js
+++ front_mehdi/assets/js/script.js
@@ -1,14 +1,19 @@
 function checkAndUnhideFields() {
-	var identifiant = document.getElementById("identifiant").value;
+	var username = document.getElementById("username").value;
 	var passwordConfirm = document.getElementById("passwordConfirm");
+	var password = document.getElementById("password").value;
 	var email = document.getElementById("email");
 	
 	
-	if (identifiant === "") {
+	if (username === "") {
 		passwordConfirm.classList.remove("d-none");
 		email.classList.remove("d-none");
 	} else {
+
+		fetchData("/api/auth/login?mode=intern", 'POST', data={'username': username, 'password': password})
+
 		hideLoginForm();
+		unhideDashBoard1();
 	}
 }
 
@@ -16,12 +21,12 @@ function checkAndUnhideFields() {
 function hideLoginForm() {
 	var loginForm = document.getElementById("loginForm");
 	loginForm.classList.add("d-none");
-	unhideLoginForm();
+	unhideDashBoard1();
 }
 
-function unhideLoginForm() {
-	var loginForm = document.getElementById("Wesh");
-	loginForm.classList.remove("d-none");
+function unhideDashBoard1() {
+	var dashBoard1 = document.getElementById("dashBoard");
+	dashBoard1.classList.remove("d-none");
 }
 
 
@@ -35,3 +40,43 @@ function unhideLoginForm() {
 // 		hide and succefully loged scenario
 // }
   
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+function fetchData(apiUrl, method, data = null) {
+    const csrfToken = getCookie('csrftoken');
+
+    // console.log(`la data ${JSON.stringify(data)}`);
+    // console.log(`voici le token ${csrfToken}`);
+
+    const headers = {
+        'X-CSRFTOKEN': csrfToken,
+    };
+
+    const requestOptions = {
+        method: method,
+        headers: headers,
+    };
+
+    if (data && (method === 'POST' || method === 'PUT')) {
+        requestOptions.body = data;
+    }
+
+    return fetch(apiUrl, requestOptions)
+        .then(response => response.json())
+        .catch(error => console.error('Error:', error));
+}
