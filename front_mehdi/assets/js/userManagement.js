8658494f76e1cef662fc5408289c067e3a6fa5f0
Mehdi Desmartin <mdesmartin@mehdis-air.home>
2024-03-14 16:09:07 +0100

    ADD: tap enter to signin or up, reorganize js

---
+++ front_mehdi/assets/js/userManagement.js
@@ -0,0 +1,109 @@
+//check at launch if logged
+(async function() {
+    const logged = await isLogged();
+    if (logged) {
+		changeScene("home");
+    } else {
+		changeScene("signIn");
+    }
+})();
+
+async function isLogged() {
+    const response = await fetch('/api/dashboard');
+    return response.status === 200;
+}
+
+function signIn() {
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
+	
+    var formData = new FormData();
+    
+    formData.append("username", username);
+    formData.append("password", password);
+
+	fetchData("/api/auth/login?mode=intern", 'POST', formData).then(
+		(data) => {
+			if (data.status === 200) {
+				console.log("Connexion réussie");
+				changeScene("home");
+                appendAlert('Connexion réussie!', 'success'); // Afficher l'alerte de succès
+			} else {
+				console.log("Erreur de connexion");
+			}
+		}
+	).catch(error => {//ca marche pas ca encore, VOIR ETHAN
+		console.error('Error:', error);
+	});
+}
+
+
+function signUp() {
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
+	// var passwordConfirm = document.getElementById("passwordConfirm");
+	var email = document.getElementById("email").value;
+	
+    var formData = new FormData();
+    
+    formData.append("username", username);
+    formData.append("password", password);
+    formData.append("email", email);
+	
+	fetchData("/api/auth/register", 'POST', formData).then(
+	(data) => {
+		if (data.status === 200) {
+			console.log("Register réussie");
+			changeScene("home");
+		} else {
+			console.log("Erreur de connexion");
+		}
+    })
+    .catch(error => {
+		console.error('Error:', error);
+    });
+}
+
+function signWith42() {
+	fetchData("/api/auth/login?mode=42")
+    .then((data) => {
+		data.json().then(
+			(dataJSON) => {
+				console.log(dataJSON)
+				window.location.href = dataJSON.url;
+			}
+			)
+		})	
+	}
+	
+	
+function signOut() {
+	fetchData("/api/auth/logout", 'POST')
+	.then(() => {
+		console.log("Deconexion réussie");
+		changeScene("signIn");
+	})
+	.catch(error => {
+		console.error('Error:', error);
+	});
+}
+
+function forgotPassword() {
+	var username = document.getElementById("username").value;
+	
+    var formData = new FormData();
+    
+    formData.append("username", username);
+	
+	fetchData("/api/auth/reset-password", 'POST', formData).then(
+	(data) => {
+		if (data.status === 200) {
+			console.log("password sent!");
+		} else {
+			console.log("Erreur de connexion");
+		}
+    })
+    .catch(error => {
+		console.error('Error:', error);
+    });
+}
