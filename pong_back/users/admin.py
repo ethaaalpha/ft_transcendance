cd9c29cda2616a19ca2612c239f02296639dc2aa
ethaaalpha <revenus-nager-0s@icloud.com>
2024-03-14 17:02:51 +0100

    match historic

--- pong_back/users/admin.py
+++ pong_back/users/admin.py
@@ -9,4 +9,8 @@ admin.site.register(Stats)
 admin.site.register(Conversation)
 admin.site.register(Message)
 admin.site.register(Room)
-admin.site.register(Match)
+
+class MatchAdmin(admin.ModelAdmin):
+    readonly_fields = ('date',)
+
+admin.site.register(Match, MatchAdmin)
