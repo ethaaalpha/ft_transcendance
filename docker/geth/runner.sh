bd1a06ae86dad5333767c0a8cf7a0c60d0a87fe6
ethaaalpha <30417812+ethaaalpha@users.noreply.github.com>
2024-03-17 21:30:49 +0000

    creating some first nodes

--- docker/geth/runner.sh
+++ docker/geth/runner.sh
@@ -1,8 +1,21 @@
+# Generating account for the node 1 (signer)
+geth account new --datadir node1 --password <(echo $NODE1_ACCOUNT_PASSWORD) | grep "Public address of the key" > node1.pass
+
+# Generating account for the node 2
+geth account new --datadir node2 --password <(echo $NODE2_ACCOUNT_PASSWORD) | grep "Public address of the key" > node2.pass
+
+NODE1_PUBLIC__ADDR=$(python /script/tools.py address node1.pass)
+NODE2_PUBLIC__ADDR=$(python /script/tools.py address node2.pass)
+
+echo $NODE1_PUBLIC__ADDR
+echo $NODE2_PUBLIC__ADDR
+
 # cat genesis.json
-geth --datadir data init genesis.json
-geth --datadir data --http --http.addr "0.0.0.0" --http.port "8545" --http.api "eth,web3,personal,net" --networkid 1234 console
+# geth --datadir data init genesis.json
+# geth --datadir data --http --http.addr "0.0.0.0" --http.port "8545" --http.api "eth,web3,personal,net" --networkid 1234 console
 
 # geth account new --datadir data --password <(echo $SIGNER_ACCOUNT_PASSWORD)
 # geth --datadir data2 --networkid 12345 --port 30305 --unlock 0x0000000000000000000000000000000000000001 --mine
 # geth --unlock 0x0000000000000000000000000000000000000001 --mine
 # geth --datadir=data/
+# geth account list --datadir signer > password
---
+++ docker/geth/tools.py
@@ -0,0 +1,41 @@
+import sys
+
+def	extract_address(content):
+	"""
+	Should be in this format
+	'Public address of the key:   0x90387a327E52C02C315445c2e19FD6B06Ba91915'
+	"""
+	l = len(content) - 1
+	while (content[l] != ' '):
+		l -= 1
+	return content[l + 1:len(content)].strip()
+
+def replace(file, a, b):
+	f = open(file, "r")
+	data = f.read()
+	data.replace(a, b)
+
+	with open(file, "r") as mf:
+		mf.write(data)
+	return ("Success")
+
+def getText(file):
+	f = open(file, "r")
+	return (f.read())
+
+def main():
+	args = sys.argv
+	if (len(args) != 3):
+		return ('Must be used with parameters !')
+	
+	match args[1]:
+		case 'address':
+			return extract_address(getText(args[2]))
+		case 'replace':
+			return replace(args[2], args[3], args[4])
+		case _:
+			return ("No match")
+
+if __name__ == "__main__":
+    print(main())
+
