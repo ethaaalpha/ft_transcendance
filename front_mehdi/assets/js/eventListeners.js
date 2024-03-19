132d0839b719ec2991ec25c58abdb1995ac8966a
Mehdi Desmartin <mdesmartin@Mehdis-MacBook-Air.local>
2024-03-19 17:13:27 +0100

    ADD: parsing input

--- front_mehdi/assets/js/eventListeners.js
+++ front_mehdi/assets/js/eventListeners.js
@@ -1,3 +1,47 @@
+// form parsing
+document.getElementById("username").addEventListener("input", function() {
+    var usernameInput = this.value;
+    var isValid = /^[A-Za-z0-9_+\-.@]+$/.test(usernameInput);
+    if (isValid && usernameInput.length >= 3 && usernameInput.length <= 32) {
+        this.classList.remove("is-invalid");
+    } else {
+        if (!this.classList.contains("is-invalid")) {
+            this.classList.add("is-invalid");
+        }
+    }
+});
+
+document.getElementById("password").addEventListener("input", function() {
+    var passwordInput = this.value;
+    if (passwordInput.length >= 5 && passwordInput.length <= 42) {
+        this.classList.remove("is-invalid");
+    } else {
+        this.classList.add("is-invalid");
+    }
+});
+
+document.getElementById("passwordConfirm").addEventListener("input", function() {
+    var confirmPasswordInput = this.value;
+    var passwordInput = document.getElementById("password").value;
+    var isValid = confirmPasswordInput === passwordInput;
+    if (isValid) {
+        this.classList.remove("is-invalid");
+    } else {
+        this.classList.add("is-invalid");
+    }
+});
+
+document.getElementById("email").addEventListener("input", function() {
+    var emailInput = this.value;
+    var isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+\w*$/.test(emailInput) && emailInput.length <= 254;
+    if (isValid) {
+        this.classList.remove("is-invalid");
+    } else {
+        this.classList.add("is-invalid");
+    }
+});
+
+// keyboad touch
 document.addEventListener("DOMContentLoaded", function() {
     const usernameInput = document.getElementById("username");
     const passwordInput = document.getElementById("password");
