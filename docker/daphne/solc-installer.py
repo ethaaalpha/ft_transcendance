6227485206208caa2d83e540e9974ebbe0393947
ethaaalpha <revenus-nager-0s@icloud.com>
2024-03-20 20:55:58 +0100

    tests around solc

--- docker/daphne/solc-installer.py
+++ docker/daphne/solc-installer.py
@@ -1,5 +1,8 @@
 from solcx import install_solc
+import solcx
 
-print('Installating solcx compiler !')
+print('Installating solcx compiler ! ')
 install_solc(version='0.8.25')
+print(solcx.get_installed_solc_versions())
+print(solcx.get_compilable_solc_versions())
 print('Installation successful !')
