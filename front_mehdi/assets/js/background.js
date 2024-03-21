420281db75a10f4905880b8afb38f92d698b5163
Mehdi Desmartin <mdesmartin@Mehdis-MacBook-Air.local>
2024-03-21 16:26:38 +0100

    ADD: Background to login page

---
+++ front_mehdi/assets/js/background.js
@@ -0,0 +1,14 @@
+  window.onload = function() {
+	var speed = 5; // Vitesse de défilement en pixels par seconde
+	var backgroundZ1 = document.querySelector('.backgroundZ1');
+	var currentPosition = 0;
+  
+	function scrollBackground() {
+	  currentPosition += speed / 60;
+	  backgroundZ1.style.backgroundPositionX = currentPosition + 'px';
+	  requestAnimationFrame(scrollBackground);
+	}
+  
+	requestAnimationFrame(scrollBackground);
+  };
+  
