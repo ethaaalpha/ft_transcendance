4e15fc2634f0c851e73f14cf22e5642d0fc8de57
Mehdi Desmartin <mdesmartin@Mehdis-MacBook-Air.local>
2024-03-29 23:06:07 +0100

    ADD: game theme need team approval

--- front_mehdi/assets/js/divCreator.js
+++ front_mehdi/assets/js/divCreator.js
@@ -1,20 +1,51 @@
-// Fonction pour générer les div avec les images carrées et les bords arrondis
+// // Fonction pour générer les div avec les images carrées et les bords arrondis
+// function genererDivsAvecImages(nbDivs) {
+// 	const conteneur = document.getElementById("modify-game-theme-menu");
+  
+// 	for (let i = 0; i < nbDivs; i++) {
+// 	  // Création de l'élément div
+// 	  const div = document.createElement("div");
+	  
+// 	  // Attribution de classes CSS
+// 	  div.classList.add("modify-game-theme-image-container");
+  
+// 	  // Création de l'élément image
+// 	  const image = document.createElement("img");
+	  
+// 	  // Attribution de la source de l'image
+// 	  image.src = "assets/images/theme-0.png";
+	  
+// 	  // Ajout des classes CSS pour les bords arrondis
+// 	  image.classList.add("modify-game-theme-image");
+  
+// 	  // Ajout de l'image à la div
+// 	  div.appendChild(image);
+  
+// 	  // Ajout de la div au conteneur
+// 	  conteneur.appendChild(div);
+// 	}
+//   }
+  
+//   // Appel de la fonction pour générer 5 divs avec des images
+//   genererDivsAvecImages(3);
+
+
 function genererDivsAvecImages(nbDivs) {
 	const conteneur = document.getElementById("modify-game-theme-menu");
   
 	for (let i = 0; i < nbDivs; i++) {
 	  // Création de l'élément div
 	  const div = document.createElement("div");
-	  
+  
 	  // Attribution de classes CSS
 	  div.classList.add("modify-game-theme-image-container");
   
 	  // Création de l'élément image
 	  const image = document.createElement("img");
-	  
-	  // Attribution de la source de l'image
-	  image.src = "assets/images/theme-0.png";
-	  
+  
+	  // Attribution de la source de l'image en utilisant l'index de la boucle pour incrémenter le chiffre dans le nom de l'image
+	  image.src = `assets/images/theme-${i}.png`;
+  
 	  // Ajout des classes CSS pour les bords arrondis
 	  image.classList.add("modify-game-theme-image");
   
@@ -26,6 +57,6 @@ function genererDivsAvecImages(nbDivs) {
 	}
   }
   
-  // Appel de la fonction pour générer 5 divs avec des images
-  genererDivsAvecImages(3);
+  // Appel de la fonction pour générer 3 divs avec des images
+  genererDivsAvecImages(6);
   
