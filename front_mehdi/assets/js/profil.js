24f941a294d5f4f237808423667c64b1470f3c1a
Mehdi Desmartin <mdesmartin@Mehdis-MacBook-Air.local>
2024-03-29 17:55:42 +0100

    Profil in progress

--- front_mehdi/assets/js/profil.js
+++ front_mehdi/assets/js/profil.js
@@ -1,19 +1,29 @@
-function updatePP(link){
-	const profilePicture = document.getElementById('profilePicture');
-	profilePicture.src = link;
+function updateProfil() {
+    fetchData('/api/dashboard')
+        .then(data => {
+            console.log(data);
+            updateProfilPicture('media/' + data['profilePicture']);
+            updateUsername(data['username']);
+        })
+        .catch(error => {
+            console.error('Error fetching user data:', error);
+        });
+}
+
+function updateProfilPicture(src) {
+    const profilePicture = document.getElementById('profilePicture');
+    if (profilePicture) {
+        profilePicture.src = src;
+    } else {
+        console.error('Element with ID "profilePicture" not found');
+    }
 }
 
 function updateUsername(username) {
-	const usernameHTML = document.getElementById('username')
-	usernameHTML.innerText = username;
-	// usernameHTML.style.fontWeight = 'bold';
-}
-
-function loadLogged() {
-	fetchData('/api/dashboard')
-		.then(data => {
-			console.log(data)
-			updatePP('media/' + data['profilePicture']);
-			updateUsername(data['username']);
-	});
+    const profilUsername = document.getElementById('profilUsername');
+    if (profilUsername) {
+        profilUsername.innerText = username;
+    } else {
+        console.error('Element with ID "username" not found');
+    }
 }
