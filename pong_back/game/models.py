749066f1b546ff55a8990a90a51010a1aa614456
ethaaalpha <revenus-nager-0s@icloud.com>
2024-03-30 22:03:30 +0100

    code clean

--- pong_back/game/models.py
+++ pong_back/game/models.py
@@ -372,13 +372,12 @@ class Room(models.Model):
 		self.closed.add(user)
 		self.save()
 
+		if self.closed.count() == self.opponents.count():
+			return self.delete()
+
 	def getRank(self, user):
 		eliminated = list(self.eliminated.all())
 		return (self.opponents.count() - eliminated.index(user))
-
-	def autodestruction(self):
-		if self.closed.count() == self.opponents.count():
-			return self.delete()
 
 	@staticmethod
 	def createRoom(owner: User, mode = Mode.CLASSIC):
@@ -476,12 +475,12 @@ class Room(models.Model):
 				# print(f"j'envoie le message end à {player.username}", file=sys.stderr)
 				room.send(player, 'end', {'room-id': room.id,'rank': room.getRank(player)})
 				room.addClosed(player)
-				return room.autodestruction()
+				return
 			if player == room.winner:
 				# print(f"j'envoie le message win à {player.username}", file=sys.stderr)
 				room.send(player, 'win', {'room-id': room.id})
 				room.addClosed(player)
-				return room.autodestruction()
+				return
 			matchsRunning = list(room.matchs.all().order_by('-creation_date')[:room.numberMatchsLastRound])
 			# print(f'on me demande un next bebe {player.username} {room.matchs.all()} | {matchsRunning}', file=sys.stderr)
 			for m in matchsRunning:
