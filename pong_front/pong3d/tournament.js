3f6e92030ce25c0ad2978e0314999aa1e8ad960d
Ethaniel Billon <ebillon@z4r5p5.42lyon.fr>
2024-04-23 15:40:59 +0200

    join tournament formular with coordination socket

---
+++ pong_front/pong3d/tournament.js
@@ -0,0 +1,195 @@
+var opacity_low = 0.4;
+var opacity_medium = 0.6;
+var opacity_high = 0.8;
+
+function unhideElement(id) {
+	let element = document.getElementById(id);
+	element.classList.remove('d-none');
+}
+
+function hideElement(id) {
+	let element = document.getElementById(id);
+	element.classList.add('d-none');
+}
+
+function changeValue(id, value) {
+	let element = document.getElementById(id);
+	element.innerHTML = value;
+}
+
+class FormTournamentEvent {
+	constructor (player, action) {
+		this.player = player;
+		this.action = action;
+		this.childElement = null;
+	}
+
+	build (parent) {
+		let parentElement = document.getElementById(parent);
+		let childElement = document.createElement('div');
+		let children = [];
+
+		childElement.classList.add('tournament-form-content-children');
+		childElement.style.setProperty('--opacity', opacity_low)
+		let spanA = document.createElement('span');
+		spanA.classList.add('tournament-name');
+		spanA.innerHTML = ' ' + this.player;
+		let spanB = document.createElement('span');
+		spanB.classList.add('tournament-text-italic');
+		spanB.innerHTML = ' just ' + this.action + ' the room';
+		
+
+		let item = document.createElement('i');
+		item.classList.add('bi');
+		if (this.action == 'left'){
+			item.classList.add('bi-dash')
+			children.push(item);
+		} else {
+			item.classList.add('bi-plus')
+			children.push(item);
+		}
+		children.push(spanA);
+		children.push(spanB);
+
+		parentElement.appendChild(childElement);
+		children.forEach(child => {
+			childElement.appendChild(child);
+		});
+
+		this.childElement = childElement;
+		return this;
+	}
+
+	getChild() {
+		return this.childElement;
+	}
+}
+
+class FormTournament {
+
+	constructor (callback) {
+		this.changeToInactive();
+		this.callback = callback
+	}
+
+	defaultValues(){
+		this.state = 0; // 0 mean first 1 mean second
+		this.roomCode = null;
+		this.historic = [];
+		this.count = 0;
+		this.max = 0;
+	}
+
+	updateCount() {
+		if (this.count < 0)
+			this.count = 0;
+		if (this.count > this.max)
+			this.count = this.max;
+		let newvalue = this.count.toString() + '/' + this.max.toString();
+		changeValue('tournament-js-count', newvalue);
+	}
+
+	updateOpacity() {
+		this.historic[0].getChild().style.setProperty('--opacity', opacity_high);
+		if (this.historic.length >= 2) {
+			this.historic[1].getChild().style.setProperty('--opacity', opacity_medium);
+		}
+
+	}
+	eventPlayer(player, newcount) {
+		if (newcount > this.count)
+			this.joinPlayer(player)
+		else
+			this.leavePlayer(player)
+	}
+	
+	joinPlayer(player) {
+		if (this.state == 0)
+			return;
+		this.count += 1;
+		this.updateCount();
+		this.historic.push(new FormTournamentEvent(player, 'join').build('tournament-form-content'));
+		this.updateOpacity();
+	}
+
+	leavePlayer(player) {
+		if (this.state == 0)
+			return;
+		this.count -= 1;
+		this.updateCount();
+		this.historic.push(new FormTournamentEvent(player, 'left').build('tournament-form-content'));
+		this.updateOpacity();
+	}
+
+	registerEventsRoom() {
+		document.getElementById('tournament-form-top-right').addEventListener("click", (event) => {
+			this.callback({'event': 'tournament', 'data': {
+				'action': 'quit',
+				'room-id': this.roomCode,
+				},
+			})
+			// finir ici en send ws
+		});
+	}
+	
+	registerEventsWait() {
+		document.getElementById('tournament-js-create').addEventListener("click", (event) => {
+			let value = document.getElementById('tournament-js-select').value;
+			this.callback({'event': 'create', 'data': {
+				'mode': `tournament${value}`
+				},
+			})
+			// finir ici en send ws
+		});
+
+		document.getElementById('tournament-js-join').addEventListener("click", (event) => {
+			let value = document.getElementById('tournament-js-code').value;
+			this.roomCode = value
+			this.callback({'event': 'tournament', 'data': {
+				'room-id': value,
+				'action': 'join',
+				},
+			})
+			// finir ici en send ws
+		});
+
+		document.getElementById('tournament-escape-button').addEventListener('click', (event) => {
+			this.callback({'event': 'tournament', 'data': {
+				'action': 'quit',
+				'room-id': this.roomCode,
+				},
+			})
+		})
+	}
+
+	changeToRoom(roomCode, max) {
+		if (roomCode != null)
+			this.roomCode = roomCode;
+		this.state = 1;
+		this.max = max;
+		unhideElement('game');
+		hideElement('tournament-a');
+		unhideElement('tournament-b');
+		this.updateCount();
+		this.registerEventsRoom();
+		changeValue('tournament-js-code-display', this.roomCode);
+	}
+
+	changeToWait() {
+		unhideElement('game');
+		hideElement('tournament-b')
+		unhideElement('tournament-a')
+		this.defaultValues();
+		this.registerEventsWait();
+	}
+
+	changeToInactive() {
+		this.defaultValues();
+		hideElement('tournament-b');
+		hideElement('tournament-a');
+		hideElement('game');
+	}
+}
+// ft.changeToWait();
+// ft.changeToRoom('JUD2JIDE', 42)
+export default FormTournament
