533769886a24583f0c1d921ffd4a04eccad38781
Mehdi Desmartin <mdesmartin@Mehdis-MacBook-Air.local>
2024-04-25 06:41:25 +0200

    FIX: send message

--- front_mehdi/assets/js/action/chat.js
+++ front_mehdi/assets/js/action/chat.js
@@ -2,14 +2,15 @@ import globalVariables from '../init.js';
 import Conversations from '../class/Conversation.js';
 import { fetchData } from '../fetch/api.js';;
 import User from '../class/User.js';
+import Connect from '../class/Connect.js';
 
 async function fetchConversations() {
 	try {
-	  const username = globalVariables.currentUser.getUsername();
-	  const data = await fetchData('/api/dashboard/conversations');
-	  globalVariables.userConversations = new Conversations(username, data.data.conversations);
+	const username = globalVariables.currentUser.getUsername();
+	const data = await fetchData('/api/dashboard/conversations');
+	globalVariables.userConversations = new Conversations(username, data.data.conversations);
 	} catch (error) {
-	  console.error('Error fetching user data:', error);
+	console.error('Error fetching user data:', error);
 	}
 }
 
@@ -18,21 +19,21 @@ function sendMessage() {
 	const content = document.getElementById("send-message-input-id").value;
 
 	// console.log(to);
-    const data = {'to': to, 'content': content};
-    if (globalVariables.activity && globalVariables.activity.socket.readyState === WebSocket.OPEN) {
-        globalVariables.activity.socket.send(JSON.stringify({
-            'event': 'chat',
-            'data': data,
-        }));
+	const data = {'to': to, 'content': content};
+	if (globalVariables.activity && globalVariables.activity.socket.readyState === WebSocket.OPEN) {
+		globalVariables.activity.socket.send(JSON.stringify({
+			'event': 'chat',
+			'data': data,
+		}));
 		globalVariables.userConversations.addMessageFromSocket(data);
-    } else {
-        console.error("Erreur lors de l'envoi du message : Websocket non connecté.");
-    }
+	} else {
+		console.error("Error sending message: Websocket not connected.");
+	}
 }
 
 function scrollMessagesToBottom() {
-    const messagesElement = document.getElementById("conversation-display-messages-id");
-    messagesElement.scrollTop = messagesElement.scrollHeight;
+	const messagesElement = document.getElementById("conversation-display-messages-id");
+	messagesElement.scrollTop = messagesElement.scrollHeight;
 }
 
 export { fetchConversations, sendMessage, scrollMessagesToBottom };
