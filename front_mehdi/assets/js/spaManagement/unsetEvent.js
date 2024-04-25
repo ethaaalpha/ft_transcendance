664783515b3b66ff0d8c7f8a25fbbc7e85a45239
Mehdi Desmartin <mdesmartin@Mehdis-MacBook-Air.local>
2024-04-25 21:59:00 +0200

    in progress event cleaning

--- front_mehdi/assets/js/spaManagement/unsetEvent.js
+++ front_mehdi/assets/js/spaManagement/unsetEvent.js
@@ -1,3 +1,4 @@
+import globalVariables from '../init.js';
 import { createChildDiv } from './div.js';
 import { sendMessage } from '../action/chat.js';
 
@@ -5,12 +6,12 @@ function unsetEventListener(eventToUnset, exception) {
 	eventToUnset.forEach(event => {
 		if (event !== exception) {
 			switch (event) {
-				// case "sign-in":
-				//     unsetEventSignIn();
-				//     break;
-				// case "sign-up":
-				//     unsetEventSignUp();
-				//     break;
+				case "sign-in":
+					unsetEventSignIn();
+					break;
+				case "sign-up":
+					unsetEventSignUp();
+					break;
 				case "conversation-list":
 					unsetEventConversationList();
 					break;
@@ -23,24 +24,6 @@ function unsetEventListener(eventToUnset, exception) {
 				case "search-contact":
 					unsetEventSearchContact();
 					break;
-				// case "profil":
-				//     unsetEventProfil();
-				//     break;
-				// case "settings":
-				//     unsetEventSettings();
-				//     break;
-				// case "modify-game-theme":
-				//     unsetEventModifyGameTheme();
-				//     break;
-				// case "modify-profil-picture":
-				//     unsetEventModifyProfilPicture();
-				//     break;
-				// case "modify-password":
-				//     unsetEventModifyPassword();
-				//     break;
-				// case "modify-email":
-				//     unsetEventModifyEmail();
-				//     break;
 				default:
 					console.log("Invalid event for unsetEventListener: ", event);
 			}
@@ -48,42 +31,115 @@ function unsetEventListener(eventToUnset, exception) {
 	});
 }
 
-function unsetEventConversationDisplay() {
-	const messageInput = document.getElementById("send-message-input-id");
+// REMOVE
+function removeEventListener(eventName, elementId) {
+	if (!globalVariables.eventListeners || !globalVariables.eventListeners[eventName]) return;
 
-	if (messageInput) {
-		messageInput.removeEventListener("keypress", sendMessage);
+	const eventHandler = globalVariables.eventListeners[eventName][elementId];
+	const element = document.getElementById(elementId);
+	if (element && eventHandler) {
+		element.removeEventListener(eventName, eventHandler);
+		delete globalVariables.eventListeners[eventName][elementId];
 	}
+}
+
+// HANDLER
+function unsetEventSignIn() {
+	removeEventListener("input", "sign-in-username");
+	removeEventListener("input", "sign-in-password");
+	removeEventListener("keypress", "sign-in-username");
+	removeEventListener("keypress", "sign-in-password");
+}
+
+function unsetEventSignUp() {
+	removeEventListener("input", "sign-up-username");
+	removeEventListener("input", "sign-up-password");
+	removeEventListener("input", "sign-up-password-confirm");
+	removeEventListener("input", "sign-up-email");
+	removeEventListener("keypress", "sign-up-username");
+	removeEventListener("keypress", "sign-up-password");
+	removeEventListener("keypress", "sign-up-password-confirm");
+	removeEventListener("keypress", "sign-up-email");
+}
+
+function unsetEventConversationDisplay() {
+	removeEventListener("keypress", "send-message-input-id");
 }
 
 function unsetEventConversationList() {
-	const searchInput = document.getElementById("conversation-list-searchbar-input-id");
-
-	if (searchInput) {
-		searchInput.removeEventListener("input", onInputFilled);
-		searchInput.removeEventListener("input", onInputCleared);
-		searchInput.removeEventListener("keypress", createChildDiv);
-	}
+	removeEventListener("input", "conversation-list-searchbar-input-id");
+	removeEventListener("keypress", "conversation-list-searchbar-input-id");
 }
 
 function unsetEventSearch() {
-	const searchInput = document.getElementById("search-searchbar-input-id");
-
-	if (searchInput) {
-		searchInput.removeEventListener("input", onInputFilled);
-		searchInput.removeEventListener("input", onInputCleared);
-		searchInput.removeEventListener("keypress", createChildDiv);
-	}
+	removeEventListener("input", "search-searchbar-input-id");
+	removeEventListener("keypress", "search-searchbar-input-id");
 }
 
 function unsetEventSearchContact() {
-	const searchInput = document.getElementById("search-contact-searchbar-input-id");
-
-	if (searchInput) {
-		searchInput.removeEventListener("input", onInputFilled);
-		searchInput.removeEventListener("input", onInputCleared);
-		searchInput.removeEventListener("keypress", createChildDiv);
-	}
+	removeEventListener("input", "search-contact-searchbar-input-id");
+	removeEventListener("keypress", "search-contact-searchbar-input-id");
 }
 
+
+
+
+
+
+
+// function unsetEventConversationDisplay() {
+// 	const messageInput = document.getElementById("send-message-input-id");
+
+// 	if (messageInput) {
+// 		messageInput.removeEventListener("keypress", sendMessage);
+// 	}
+// }
+
+// function unsetEventConversationList() {
+// 	const searchInput = document.getElementById("conversation-list-searchbar-input-id");
+
+// 	if (searchInput) {
+// 		searchInput.removeEventListener("input", onInputFilled);
+// 		searchInput.removeEventListener("input", onInputCleared);
+// 		searchInput.removeEventListener("keypress", createChildDiv);
+// 	}
+// }
+
+// function unsetEventSearch() {
+// 	const searchInput = document.getElementById("search-searchbar-input-id");
+
+// 	if (searchInput) {
+// 		searchInput.removeEventListener("input", onInputFilled);
+// 		searchInput.removeEventListener("input", onInputCleared);
+// 		searchInput.removeEventListener("keypress", createChildDiv);
+// 	}
+// }
+
+// function unsetEventSearchContact() {
+// 	const searchInput = document.getElementById("search-contact-searchbar-input-id");
+
+// 	if (searchInput) {
+// 		searchInput.removeEventListener("input", onInputFilled);
+// 		searchInput.removeEventListener("input", onInputCleared);
+// 		searchInput.removeEventListener("keypress", createChildDiv);
+// 	}
+// }
+
 export { unsetEventListener }
+
+
+// REMOVE ALL
+// function removeAllglobalVariables.EventListeners() {
+//     for (const eventName in globalVariables.eventListeners) {
+//         const listeners = globalVariables.eventListeners[eventName];
+//         for (const elementId in listeners) {
+//             const eventHandler = listeners[elementId];
+//             const element = document.getElementById(elementId);
+//             if (element) {
+//                 element.removeEventListener(eventName, eventHandler);
+//             }
+//         }
+//     }
+
+// 	globalVariables.eventListeners = {};
+// }
