d8d1c5221efa8852debc9bb326bed654b5cb2c88
ethaaalpha <revenus-nager-0s@icloud.com>
2024-05-09 14:20:41 +0200

    background runner

--- front/default/assets/js/action/background.js
+++ front/default/assets/js/action/background.js
@@ -1,17 +1,36 @@
 import {sleep} from "/static/pong3d/utilsPong.js"
+import globalVariables from "/static/default/assets/js/init.js"
 
-function backgroundRunner() {
-	var speed = 50; // Vitesse de défilement en pixels par seconde
-	var backgroundZ1 = document.querySelector('.backgroundZ1');
-	var currentPosition = 0;
-	
-	// async function scrollBackground() {
-	// 	currentPosition += speed / 60;
-	// 	backgroundZ1.style.backgroundPositionX = currentPosition + 'px';
-	// 	await sleep(200)
-	// 	requestAnimationFrame(scrollBackground);
-	// }
-	// requestAnimationFrame(scrollBackground);
+function backgroundRunner(active = true) {
+    var speed = 50; 
+    var backgroundZ1 = document.querySelector('.backgroundZ1');
+    var currentPosition = 0;
+	var animationId = null;
+
+    async function scrollBackground() {
+		if (!globalVariables.backgroundRunner)
+			return
+        currentPosition += speed / 60;
+        backgroundZ1.style.backgroundPositionX = currentPosition + 'px';
+        await sleep(135);
+		animationId = requestAnimationFrame(scrollBackground);
+
+    }
+
+    function toggleAnimation() {
+        if (active) {
+			if (!globalVariables.backgroundRunner)
+				globalVariables.backgroundRunner = true;
+            	animationId = requestAnimationFrame(scrollBackground);
+        } else {
+			if (globalVariables.backgroundRunner) {
+				globalVariables.backgroundRunner = false;
+            	cancelAnimationFrame(globalVariables.backgroundRunner);
+			}
+        }
+    }
+
+    toggleAnimation();
 }
 
 export { backgroundRunner };
