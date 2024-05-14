761ce0ee34c1dfc31b108b9a2f72a00e98b7a629
ethaaalpha <revenus-nager-0s@icloud.com>
2024-05-14 11:53:45 +0200

    env file builder

--- back/pong_back/settings.py
+++ back/pong_back/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
+        'HOST': 'postgresql',
         'PORT': '5432',
     }
 }
